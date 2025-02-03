208dfa30e737b1a1236165bbd11f5d5e06e6e414
= <=>
2025-02-03 12:37:41 +0000

    Lab solution - Néstor

--- index.js
+++ index.js
@@ -1,17 +1,75 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+    this.items = []
+    this.length = 0
+  }
 
-  get(pos) {}
+  add(item) {
 
-  max() {}
+    this.items.push(item)
+    //this.items.sort()
+    this.items = this.items.sort(function (a, b) {  return a - b;  });
+    this.length = this.items.length
 
-  min() {}
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if(this.length > pos) { return this.items[pos] }
+    else { throw new Error("OutOfBounds")}
+
+  }
+
+  max() {
+
+    if(this.length == 0) { throw new Error("EmptySortedList")}
+
+    else {return this.items[this.length - 1]}
+
+  }
+
+  min() {
+
+    if(this.length == 0) { throw new Error("EmptySortedList")}
+
+    else {return this.items[0]}
+
+  }
+
+  sum() {
+
+    let sumatoria = 0
+    this.items.forEach(number => {
+
+      sumatoria = sumatoria + number
+
+    });
+
+    return sumatoria
+
+  }
+
+  avg() {
+
+    if(this.length == 0) { throw new Error("EmptySortedList")}
+
+    else {
+
+      let sumatoria = 0
+      let media = 0
+      this.items.forEach(number => {
+  
+        sumatoria = sumatoria + number
+  
+      });
+      
+    media = sumatoria/this.length
+    return media
+  
+    }
+
+  }
 }
 
 module.exports = SortedList;
